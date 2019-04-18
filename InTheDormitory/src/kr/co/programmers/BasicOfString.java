d7b3a22ca748545eb2b9410a94cb88956342df76
ukrinseop <42711023+ukrinseop@users.noreply.github.com>
2019-04-18 10:09:58 +0900

    BASIC OF STRING

---
+++ InTheDormitory/src/kr/co/programmers/BasicOfString.java
@@ -0,0 +1,27 @@
+package kr.co.programmers;
+
+//문자열 s의 길이가 4 혹은 6이고,
+//숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요.
+//예를 들어 s가 a234이면 False를 리턴하고 1234라면 True를 리턴하면 됩니다.
+//
+//제한 사항
+//s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+
+
+public class BasicOfString {
+	
+	public boolean solution(String s) {
+		boolean answer = true;
+		
+		      
+		      
+		return answer;
+	}
+
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
